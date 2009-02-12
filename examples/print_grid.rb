20a2ffc902f97451ac6052a302341a9a9a739a9a
Andrew O'Brien <obrien.andrew@gmail.com>
2009-02-12 10:14:14 -0500

    Cleaned up example

--- examples/print_grid.rb
+++ examples/print_grid.rb
@@ -8,21 +8,10 @@ Prawn::Document.generate(File.join(File.dirname(__FILE__), 'grid.pdf')#,
     # :bottom_margin => 0
   ) do |p|
   p.define_grid(:columns => 5, :rows => 8, :gutter => 10)
-  puts "w: #{p.bounds.width}, h: #{p.bounds.height}"
-  
-  p.fill_color = "ff0000"
-  p.fill { p.circle_at([440.0, 547.5], :radius => 10)}
-  
-  p.fill_color = "0000ff"
-  p.fill { p.circle_at([440.0, 710.0], :radius => 10)}
-  
   
   p.grid.rows.times do |i|
     p.grid.columns.times do |j|
       b = p.grid(i,j)
-      p.fill_color = "333333"
-      p.fill { p.circle_at(b.top_left, :radius => 10)}
-      puts "i: #{i}, j: #{j}, tl: #{b.top_left.inspect}, b.w: #{b.width}, b.h: #{b.height}, name: #{b.name}"
       p.bounding_box b.top_left, :width => b.width, :height => b.height do
         p.text b.name
         p.stroke do
