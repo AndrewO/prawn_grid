9729eb644ba8c7e05cf7ce9ae88961e8cb26f0d8
Andrew O'Brien <obrien.andrew@gmail.com>
2009-02-12 11:49:58 -0500

    Multigrid working in example.  Will write some tests to confirm.

---
+++ examples/multi_boxes.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+require File.join(File.dirname(__FILE__), '..', 'lib', 'prawn_grid')
+
+Prawn::Document.generate(File.join(File.dirname(__FILE__), 'multi_boxes.pdf')#, 
+    # :left_margin => 0,
+    # :right_margin => 0,
+    # :top_margin => 0,
+    # :bottom_margin => 0
+  ) do |p|
+  p.define_grid(:columns => 5, :rows => 8, :gutter => 10)
+  
+  p.grid.rows.times do |i|
+    p.grid.columns.times do |j|
+      p.grid(i,j).bounding_box do
+        p.text p.grid(i,j).name
+        p.stroke_color = "cccccc"
+        p.stroke do
+          p.rectangle(p.bounds.top_left, p.bounds.width, p.bounds.height)
+        end
+      end
+    end
+  end
+  
+  g = p.grid([0,0], [1,1])
+  g.bounding_box do
+    p.text g.name
+    p.stroke_color = "333333"
+    p.stroke do
+      p.rectangle(p.bounds.top_left, p.bounds.width, p.bounds.height)
+    end
+  end
+
+  g = p.grid([3,0], [3,3])
+  g.bounding_box do
+    p.text g.name
+    p.stroke_color = "333333"
+    p.stroke do
+      p.rectangle(p.bounds.top_left, p.bounds.width, p.bounds.height)
+    end
+  end
+
+  g = p.grid([5,1], [4,0])
+  g.bounding_box do
+    p.text g.name
+    p.stroke_color = "333333"
+    p.stroke do
+      p.rectangle(p.bounds.top_left, p.bounds.width, p.bounds.height)
+    end
+  end
+
+  g = p.grid([5,3], [4,4])
+  g.bounding_box do
+    p.text g.name
+    p.stroke_color = "333333"
+    p.stroke do
+      p.rectangle(p.bounds.top_left, p.bounds.width, p.bounds.height)
+    end
+  end
+
+end
--- lib/prawn_grid.rb
+++ lib/prawn_grid.rb
@@ -17,7 +17,7 @@ module Prawn::Gridable
     else
       g1, g2 = args
       if g1.class == Array && g2.class == Array && g1.length == 2 && g2.length == 2
-        multi_box(g1, g2)
+        multi_box(single_box(*g1), single_box(*g2))
       else
         single_box(g1, g2)
       end
@@ -60,31 +60,11 @@ module Prawn::Gridable
     end
     
     def name
-      "#{@i.to_s}, #{@j.to_s}"
-    end
-    
-    def top_left
-      @top_left ||= [((width + gutter) * @j.to_f), total_height - ((height + gutter) * @i.to_f)]
-    end
-    
-    def top_right
-      @top_right ||= [top_left[0] + width, top_left[1]]
-    end
-    
-    def bottom_left
-      @bottom_left ||= [top_left[0], top_left[1] - height]
-    end
-    
-    def bottom_right
-      @bottom_right ||= [top_left[0] + width, top_left[1] - height]
+      "#{@i.to_s},#{@j.to_s}"
     end
     
     def total_height
       pdf.bounds.height.to_f
-    end
-    
-    def grid
-      pdf.grid
     end
     
     def width
@@ -99,14 +79,115 @@ module Prawn::Gridable
       grid.gutter.to_f
     end
     
+    def left
+      @left ||= (width + gutter) * @j.to_f
+    end
+    
+    def right
+      @right ||= left + width
+    end
+    
+    def top
+      @top ||= total_height - ((height + gutter) * @i.to_f)
+    end
+    
+    def bottom
+      @bottom ||= top - height
+    end
+    
+    def top_left
+      [left, top]
+    end
+    
+    def top_right
+      [right, top]
+    end
+    
+    def bottom_left
+      [left, bottom]
+    end
+    
+    def bottom_right
+      [right, bottom]
+    end
+    
     def bounding_box(&blk)
       pdf.bounding_box(top_left, :width => width, :height => height, &blk)
+    end
+    
+    private
+    def grid
+      pdf.grid
+    end
+  end
+  
+  class MultiBox < Box
+    def initialize(pdf, b1, b2)
+      @pdf = pdf
+      @bs = [b1, b2]
+    end
+    
+    def name
+      @bs.map {|b| b.name}.join(":")
+    end
+    
+    def total_height
+      @bs[0].total_height
+    end
+
+    def width
+      right_box.right - left_box.left
+    end
+    
+    def height
+      top_box.top - bottom_box.bottom
+    end
+    
+    def gutter
+      @bs[0].gutter
+    end
+    
+    def left
+      left_box.left
+    end
+    
+    def right
+      right_box.right
+    end
+    
+    def top
+      top_box.top
+    end
+    
+    def bottom
+      bottom_box.bottom
+    end
+    
+    private
+    def left_box
+      @left_box ||= @bs.min {|a,b| a.left <=> b.left}
+    end
+    
+    def right_box
+      @right_box ||= @bs.max {|a,b| a.right <=> b.right}
+    end
+    
+    def top_box
+      @top_box ||= @bs.max {|a,b| a.top <=> b.top}
+    end
+    
+    def bottom_box
+      @bottom_box ||= @bs.min {|a,b| a.bottom <=> b.bottom}
     end
   end
   
   private
   def single_box(i, j)
     Box.new(self, i, j)
+  end
+  
+  def multi_box(b1, b2)
+    MultiBox.new(self, b1, b2)
   end
 end
 
